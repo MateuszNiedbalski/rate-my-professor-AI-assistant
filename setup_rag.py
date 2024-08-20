1385b108c99bbff31f34d79c8bceefc22d2baaa0
MateuszNiedbalski <mateuszniedbalski13@gmail.com>
2024-08-20 14:24:14 +0100

    embedding not working

---
+++ setup_rag.py
@@ -0,0 +1,53 @@
+from dotenv import load_dotenv
+load_dotenv()
+from pinecone import Pinecone, ServerlessSpec
+import google.generativeai as genai
+import os
+import json
+
+
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+
+# Create a Pinecone index
+pc.create_index(
+    name="rag",
+    dimension=1408,
+    metric="cosine",
+    spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+)
+
+# Load the review data
+data = json.load(open("reviews.json"))
+
+processed_data = []
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+
+# Create embeddings for each review
+for review in data["reviews"]:
+    response = genai.embed_content(
+        content=review['review'], model="models/text-embedding-004"
+    )
+    embedding = response.data[0].embedding
+    processed_data.append(
+        {
+            "values": embedding,
+            "id": review["professor"],
+            "metadata":{
+                "review": review["review"],
+                "subject": review["subject"],
+                "stars": review["stars"],
+            }
+        }
+    )
+
+# Insert the embeddings into the Pinecone index
+index = pc.Index("rag")
+upsert_response = index.upsert(
+    vectors=processed_data,
+    namespace="ns1",
+)
+print(f"Upserted count: {upsert_response['upserted_count']}")
+
+# Print index statistics
+print(index.describe_index_stats())
